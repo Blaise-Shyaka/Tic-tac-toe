01347ff84751fa9b857d370b2c72e2b0e74b0981
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2020-09-25 19:02:44 +0200

    Add loop to repeatedly ask for player moves until the game is a win or a draw

--- bin/main.rb
+++ bin/main.rb
@@ -12,28 +12,39 @@ puts 'Do you want to see game instructions? [y/n]'
 response = gets.chomp
 # 2. If yes, print game instructions! If not, proceed with the following step.
 puts 'Instructions' if response == 'y'
-# 3. Print the tic-tac-toe board and prompt the first user for their move
-puts "
+
+# 3. Define a dummy winning condition (NOT the real game logic)
+def winner(move)
+    winning_move = 'e'
+    return winning_move == move
+end
+
+# 4. Repeatedly ask the players to make moves until the winning condition is met or it's a draw
+rounds = 0
+loop do
+
+    puts "
  _________________
 |__a__|__b__|__c__|
 |__d__|__e__|__f__|
 |__g__|__h__|__i__|
 
 "
-puts "#{player1}, Please choose a move by typing one of the letters"
-player1_move = gets.chomp
-# 4. Check if the move is a win, update the board and print the updated board
-puts "You won if #{player1_move} is a winning move"
-puts "
- _________________
-|__a__|__b__|__c__|
-|__d__|__e__|__f__|
-|__g__|__h__|__i__|
+    puts "#{player1}, Please choose a move by typing one of the letters"
+    player1_move = gets.chomp.downcase 
+    if winner player1_move
+        puts "#{player1}, Congratulations! You won."
+        break
+    end
 
-"
-# 5. Prompt the second user for their move
-puts "#{player2}, Please choose a move by typing one of the letters"
-player2_move = gets.chomp
-puts "You won if #{player2_move} is a winning move"
-# 6. Check if the move is a win, update the board and print the updated board
-# 7. Repeat the steps from 3 to 6, until it's a win or draw
+    break puts "Game is a draw!" if !winner(player1_move) && rounds == 4
+
+    puts "#{player2}, Please choose a move by typing one of the letters"
+    player2_move = gets.chomp.downcase
+    if winner player2_move
+        puts "#{player2}, Congratulations! You won."
+        break
+    end
+
+    rounds += 1
+end
