8c1e65f9857a20889b512410a5b4efb214195def
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2020-09-29 13:04:36 +0200

    resolve rubocop conflicts

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,31 +1,29 @@
+# Description/Explanation of the TicTacToe class
 class TicTacToe
   attr_accessor :board, :winning_moves
 
   def initialize
-    @board = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
-		@winning_moves = {}
-	end
+    @board = %w[a b c d e f g h i]
+    @winning_moves = {}
+  end
 
-	def update_board(player_sign, input)
-		self.board[self.board.index(input)] = player_sign
+  def update_board(player_sign, input)
+    board[board.index(input)] = player_sign
+    self.winning_moves = {  move1: [board[0], board[1], board[2]],
+                            move2: [board[3], board[4], board[5]],
+                            move3: [board[6], board[7], board[8]],
+                            move4: [board[0], board[3], board[6]],
+                            move5: [board[1], board[4], board[7]],
+                            move6: [board[2], board[5], board[8]],
+                            move7: [board[0], board[4], board[8]],
+                            move8: [board[2], board[4], board[6]] }
+  end
 
-		self.winning_moves = {
-			move1: [self.board[0], self.board[1], self.board[2]],
-      move2: [self.board[3], self.board[4], self.board[5]],
-      move3: [self.board[6], self.board[7], self.board[8]],
-      move4: [self.board[0], self.board[3], self.board[6]],
-      move5: [self.board[1], self.board[4], self.board[7]],
-      move6: [self.board[2], self.board[5], self.board[8]],
-      move7: [self.board[0], self.board[4], self.board[8]],
-      move8: [self.board[2], self.board[4], self.board[6]]
-		}
-	end
+  def winner_move
+    winning_moves.value?(%w[X X X]) || winning_moves.value?(%w[O O O])
+  end
 
-	def winner_move
-		return self.winning_moves.has_value?(["X", "X", "X"]) || self.winning_moves.has_value?(["O", "O", "O"])
-	end
-	
-	def valid_move? input
-		(input != 'x' || input != 'o') && (self.board.include? input)
+  def valid_move?(input)
+    (input != 'x' || input != 'o') && (board.include? input)
   end
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,3 +1,4 @@
+# Description/Explanation of the Player class
 class Player
     attr_accessor :player_sign, :name
     def initialize
