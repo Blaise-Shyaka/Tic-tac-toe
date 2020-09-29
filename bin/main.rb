0d8931636a4b84995ee8f4c4b8cfc10474dd029e
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2020-09-29 11:24:05 +0200

    resolve merge conflicts

--- bin/main.rb
+++ bin/main.rb
@@ -1,60 +1,67 @@
 #!/usr/bin/env ruby
+require_relative '../lib/game_logic'
 
-# rubocop:disable Lint/UselessAssignment
-
+game = TicTacToe.new
 # 1. Prompt both users to give their names
 puts "Player 1, what's your name?"
-player1 = gets.chomp
+game.player1 = gets.chomp
 puts '=============================='
 puts "Awesome! Player 2, what's your name?"
-player2 = gets.chomp
+game.player2 = gets.chomp
 puts '=============================='
-puts "Welcome #{player1} and #{player2}"
+puts "Welcome #{game.player1} and #{game.player2}"
 puts 'Do you want to see game instructions? [y/n]'
 response = gets.chomp
 # 2. If yes, print game instructions! If not, proceed with the following step.
 puts 'Instructions' if response == 'y'
 
-# 3. Define a dummy winning condition (NOT the real game logic)
-winner = false
-
-def announce_winner(player)
-  # This function declares the winner
-
-  puts "#{player}, Congratulations! You won."
-end
-
-# 4. Repeatedly ask the players to make moves till the win/draw condition is met
 rounds = 0
 loop do
-  winner = true if rounds == 3
-
-  board = "
+  puts rounds
+  puts "
   _________________
- |__a__|__b__|__c__|
- |__d__|__e__|__f__|
- |__g__|__h__|__i__|
+ |__#{game.board[0]}__|__#{game.board[1]}__|__#{game.board[2]}__|
+ |__#{game.board[3]}__|__#{game.board[4]}__|__#{game.board[5]}__|
+ |__#{game.board[6]}__|__#{game.board[7]}__|__#{game.board[8]}__|
 
  "
-  puts board
-  puts "#{player1}, Please choose a move by typing one of the letters"
+  puts "#{game.player1}, Please choose a move by typing one of the letters"
   player1_move = gets.chomp.downcase
-  if winner
-    announce_winner(player1)
+  while (!game.valid_move? player1_move)
+    puts "Wrong input #{game.player1}, Try again!"
+    player1_move = gets.chomp.downcase
+  end
+  puts "Player one's move is #{player1_move}"
+  game.update_board(game.player1, player1_move)
+  puts game.winning_moves
+  if game.winner_move
+    puts "Player1, you won"
     break
   end
+  game.update_board(game.player1, player1_move)
+  #puts "player1 moves: #{game.player1_moves}"
 
-  break puts 'Game is a draw!' if !winner && rounds == 4
+  break puts 'Game is a draw!' if (rounds >= 4) && !game.winner_move
+  puts "
+  _________________
+ |__#{game.board[0]}__|__#{game.board[1]}__|__#{game.board[2]}__|
+ |__#{game.board[3]}__|__#{game.board[4]}__|__#{game.board[5]}__|
+ |__#{game.board[6]}__|__#{game.board[7]}__|__#{game.board[8]}__|
 
-  puts board
-  puts "#{player2}, Please choose a move by typing one of the letters"
+ "
+  puts "#{game.player2}, Please choose a move by typing one of the letters"
   player2_move = gets.chomp.downcase
-  if winner
-    announce_winner(player2)
+  while (!game.valid_move? player2_move)
+    puts "Wrong input #{game.player2}, Try again!"
+    player2_move = gets.chomp.downcase
+  end
+  game.update_board(game.player2, player2_move)
+  puts game.winning_moves
+  if game.winner_move
+    puts "Player2, you won"
     break
   end
-
+  game.update_board(game.player2, player2_move)
+  #puts "player2 moves: #{game.player2_moves}"
   rounds += 1
 end
-
-# rubocop:enable Lint/UselessAssignment
