9204a2d5ae62abace078e540a12badeb6cae4d56
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2020-09-29 13:27:53 +0200

    remove unneccessary space in winning moves hash

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -10,14 +10,14 @@ class TicTacToe
 
   def update_board(player_sign, input)
     board[board.index(input)] = player_sign
-    self.winning_moves = {  move1: [board[0], board[1], board[2]],
-                            move2: [board[3], board[4], board[5]],
-                            move3: [board[6], board[7], board[8]],
-                            move4: [board[0], board[3], board[6]],
-                            move5: [board[1], board[4], board[7]],
-                            move6: [board[2], board[5], board[8]],
-                            move7: [board[0], board[4], board[8]],
-                            move8: [board[2], board[4], board[6]] }
+    self.winning_moves = { move1: [board[0], board[1], board[2]],
+                           move2: [board[3], board[4], board[5]],
+                           move3: [board[6], board[7], board[8]],
+                           move4: [board[0], board[3], board[6]],
+                           move5: [board[1], board[4], board[7]],
+                           move6: [board[2], board[5], board[8]],
+                           move7: [board[0], board[4], board[8]],
+                           move8: [board[2], board[4], board[6]] }
   end
 
   def winner_move
