5d1d794659ace6cb1a6a5ce4270911358143efad
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2020-10-08 18:59:59 +0200

    fix rubocop warnings

--- spec/game_logic_spec.rb
+++ spec/game_logic_spec.rb
@@ -11,42 +11,42 @@ describe 'TicTacToe' do
     it 'Should have a warning instance variable with nil as a value' do
       expect(game.warning).to eq(nil)
     end
-	end
-	
-	describe '#update_board' do
-		it 'Should replace the selected letter with the player sign' do
-			input = 'a'
-			player_sign = 'X'
-			game.update_board(player_sign, input)
-			expect(game.board[0]).to eq(player_sign)
-		end
+  end
 
-		it ':move2 key in the winning_moves method should include the player_sign' do
-			input = 'e'
-			player_sign = 'O'
-			game.update_board(player_sign, input)
-			expect(game.winning_moves[:move2]).to include(player_sign)
-		end
+  describe '#update_board' do
+    it 'Should replace the selected letter with the player sign' do
+      input = 'a'
+      player_sign = 'X'
+      game.update_board(player_sign, input)
+      expect(game.board[0]).to eq(player_sign)
+    end
 
-		it ':move5 key in the winning_moves method should include the player_sign' do
-			input = 'e'
-			player_sign = 'O'
-			game.update_board(player_sign, input)
-			expect(game.winning_moves[:move5]).to include(player_sign)
-		end
+    it ':move2 key in the winning_moves method should include the player_sign' do
+      input = 'e'
+      player_sign = 'O'
+      game.update_board(player_sign, input)
+      expect(game.winning_moves[:move2]).to include(player_sign)
+    end
 
-		it ':move7 key in the winning_moves method should include the player_sign' do
-			input = 'e'
-			player_sign = 'O'
-			game.update_board(player_sign, input)
-			expect(game.winning_moves[:move7]).to include(player_sign)
-		end
+    it ':move5 key in the winning_moves method should include the player_sign' do
+      input = 'e'
+      player_sign = 'O'
+      game.update_board(player_sign, input)
+      expect(game.winning_moves[:move5]).to include(player_sign)
+    end
 
-		it ':move8 key in the winning_moves method should include the player_sign' do
-			input = 'e'
-			player_sign = 'O'
-			game.update_board(player_sign, input)
-			expect(game.winning_moves[:move8]).to include(player_sign)
-		end
+    it ':move7 key in the winning_moves method should include the player_sign' do
+      input = 'e'
+      player_sign = 'O'
+      game.update_board(player_sign, input)
+      expect(game.winning_moves[:move7]).to include(player_sign)
+    end
+
+    it ':move8 key in the winning_moves method should include the player_sign' do
+      input = 'e'
+      player_sign = 'O'
+      game.update_board(player_sign, input)
+      expect(game.winning_moves[:move8]).to include(player_sign)
+    end
   end
 end
